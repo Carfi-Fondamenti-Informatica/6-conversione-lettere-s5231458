04983cc30c717e99ae8af7927dc34402caca3cd5
Marco Badin <91483735+s5231458@users.noreply.github.com>
2021-12-01 12:51:08 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,9 +1,5 @@
-//
-// Created by badin on 30/11/2021.
-//
+#ifndef ESER6_LIB_H
+#define ESER6_LIB_H
 
-#ifndef ESERCITAZIONE6_LIB_H
-#define ESERCITAZIONE6_LIB_H
-
-#endif //ESERCITAZIONE6_LIB_H
+#endif //ESER6_LIB_H
 bool conversione (char &);
