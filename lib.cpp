bcb96f9e0eddf4594a329da834039272337dd35c
Marco Badin <91483735+s5231458@users.noreply.github.com>
2021-12-01 12:43:25 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,28 @@
+//
+// Created by badin on 30/11/2021.
+//
+
+#include "Lib.h"
+
+bool conversione(char &a){
+    bool t=true;
+    if(a>=65 and a<=90 or a>=97 and a<=122){
+        t=true;
+        if (a>=65 and a<=90) {
+            a=a+32;
+            return &a;
+        }else{
+            a=a-32;
+            return &a;
+
+        }
+
+
+    }else{
+        t=false;
+    }
+    return t;
+
+}
+
 
